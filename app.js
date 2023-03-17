1015441703cf6711ac0992325ba175f4e272e6f4
Yash dhadve <yashdhadwe@gmail.com>
2023-03-17 21:09:38 +0530

    Base was created

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.listen(4000)
+
+// middleware and static files
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+
+
+app.get('/', (req, res) => {
+    res.render('index', { title : 'HOME'})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {title : 'About'})
+})
+
+app.get('/Wishlist', (req, res) => {
+    res.render('Wishlist', {title : 'Wishlist'})
+})
+
+app.use((req, res) => {
+    res.status(404).render('404', {title: 'ERROR'})
+})
